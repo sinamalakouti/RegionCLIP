89bc3711e62cd9da33cb0dd5b2dc42ee487b0fb8
sina malakouti <siinamalakouti@gmail.com>
2023-01-24 18:59:03 -0500

    Update rcnn.py

--- detectron2/modeling/meta_arch/rcnn.py
+++ detectron2/modeling/meta_arch/rcnn.py
@@ -209,10 +209,11 @@ class GeneralizedRCNN(nn.Module):
 
             joint_features = student_features @ teacher_features.t()
             n = len(teacher_features)
+            ground_truth = torch.arange(n, dtype=torch.long, device=self.device)
             print("n isssssssssssss   ", n)
             print(ground_truth)
             print(joint_features.shape)
-            ground_truth = torch.arange(n, dtype=torch.long, device=self.device)
+
             loss_fn = nn.CrossEntropyLoss()
             loss = loss_fn(joint_features, ground_truth)
             return loss
