7b3915a0a13dc4ad026dd71d159883101142d5b5
sina malakouti <siinamalakouti@gmail.com>
2023-01-31 14:39:39 -0500

    update

--- detectron2/modeling/meta_arch/rcnn.py
+++ detectron2/modeling/meta_arch/rcnn.py
@@ -195,11 +195,11 @@ class GeneralizedRCNN(nn.Module):
 
             with torch.no_grad():
                 prefix_src = self.backbone.attnpool(self.backbone(images_src)['res5'])
-                teacher_features = generate_first_feature_caption(prefix_src, clipcap_model.to(self.device), 40)
+                teacher_features = generate_feature_caption(prefix_src, clipcap_model.to(self.device), 40)
                 teacher_features = torch.stack(teacher_features, 0)
 
             prefix_trgt = self.backbone.attnpool(self.backbone(images_target)['res5'])
-            student_features = generate_first_feature_caption(prefix_trgt, clipcap_model.to(self.device), 40)
+            student_features = generate_feature_caption(prefix_trgt, clipcap_model.to(self.device), 40)
             student_features = torch.stack(student_features, 0)
 
             # loss, captions = unsupervised_loss(prefix_src, prefix_trgt, clipcap_model.to(self.device), 40)
