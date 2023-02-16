7f1729dea6439078bb0825672b3a04b4a95c5fac
sina malakouti <siinamalakouti@gmail.com>
2023-02-16 12:15:13 -0500

    Update rcnn.py

--- detectron2/modeling/meta_arch/rcnn.py
+++ detectron2/modeling/meta_arch/rcnn.py
@@ -206,18 +206,21 @@ class GeneralizedRCNN(nn.Module):
 
 
             prefix_src = self.backbone.attnpool(self.backbone(images_src)['res5'])
-            teacher_features = generate_first_feature_caption(prefix_src, clipcap_model.to(self.device), 40)
-            teacher_features = torch.stack(teacher_features, 0)
+            # teacher_features = generate_first_feature_caption(prefix_src, clipcap_model.to(self.device), 40)
+            # teacher_features = torch.stack(teacher_features, 0)
 
             prefix_trgt = self.backbone.attnpool(self.backbone(images_target)['res5'])
-            student_features = generate_first_feature_caption(prefix_trgt, clipcap_model.to(self.device), 40)
-            student_features = torch.stack(student_features, 0)
+            # student_features = generate_first_feature_caption(prefix_trgt, clipcap_model.to(self.device), 40)
+            # student_features = torch.stack(student_features, 0)
 
             # loss, captions = unsupervised_loss(prefix_src, prefix_trgt, clipcap_model.to(self.device), 40)
             # loss, captions = unsupervised_feature_loss(prefix_src, prefix_trgt, clipcap_model.to(self.device), 40)
+            # tec
+            # teacher_features = teacher_features.squeeze(1)
+            # student_features = student_features.squeeze(1)
 
-            teacher_features = teacher_features.squeeze(1)
-            student_features = student_features.squeeze(1)
+            teacher_features = prefix_src
+            student_features = prefix_trgt
             del images_src
             # del prefix_src
             del images_target
