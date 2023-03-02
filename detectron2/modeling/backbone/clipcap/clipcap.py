35cf5d90c9c981dded37f3b835d71e93591bc8ae
sina malakouti <siinamalakouti@gmail.com>
2023-03-02 14:08:41 -0500

    Update clipcap.py

--- detectron2/modeling/backbone/clipcap/clipcap.py
+++ detectron2/modeling/backbone/clipcap/clipcap.py
@@ -563,14 +563,17 @@ def generate_first_feature_caption(prefix, model: ClipCaptionModel, prefix_lengt
     break_flag = False
     out_features = None
     res = []
-    for p in model.parameters():
-            p.requires_grad = False
+
     for entry_idx in range(len(embed)):
         generated = embed[entry_idx].unsqueeze(0)
         tokens = None
         break_flag = False
         out_features = None
+
+
         for i in range(entry_length):
+
+
             # print(i)
             features = model.gpt(inputs_embeds=generated)
 
@@ -592,21 +595,163 @@ def generate_first_feature_caption(prefix, model: ClipCaptionModel, prefix_lengt
             logits[:, indices_to_remove] = filter_value
             next_token = torch.argmax(logits, -1).unsqueeze(0)
             next_token_embed = model.gpt.transformer.wte(next_token)
-
+            if tokens is None:
+                tokens = next_token
+            else:
+                tokens = torch.cat((tokens, next_token), dim=1)
             generated = torch.cat((generated, next_token_embed), dim=1)
 
             if stop_token_index == next_token.item():
                 res.append(out_features[:, -1, :])
                 out_features = None
                 break
+
+        output_list = list(tokens.squeeze().cpu().numpy())
+        output_text = model.tokenizer.decode(output_list)
+        generated_list.append(output_text)
         if out_features is not None:
             res.append(out_features[:, -1, :])
 
-    return res
+    return res,  generated_list
 
-def generate_first_feature_lang(prefix, model: ClipCaptionModel, prefix_length=10):
+# def prompt_consistency_loss(images):
+#     classes = ['aeroplane', 'bird', 'bicycle', 'boat', 'bottle', 'bus', 'car', 'cat',
+#                'chair', 'cow', 'dining table', 'dog', 'horse', 'motorbike', 'person', 'potted plant',
+#                'sheep', 'sofa', 'train', 'tv/monitor']
+#
+#     prompts = ['image of ' + c for c in classes]
+#
+#     clip_model, clip_preprocess = clip.load("RN50", device='cpu')
+#
+#         self.clip_model.visual = None
+def generate2(
+        model,
+        tokenizer,
+        tokens=None,
+        prompt=None,
+        embed=None,
+        entry_count=1,
+        entry_length=67,  # maximum number of words
+        top_p=0.8,
+        temperature=1.,
+        stop_token: str = '.',
+):
+    model.eval()
+    generated_num = 0
+    generated_list = []
+    stop_token_index = tokenizer.encode(stop_token)[0]
+    filter_value = -float("Inf")
+    device = next(model.parameters()).device
+
+    with torch.no_grad():
+
+        for entry_idx in trange(entry_count):
+            if embed is not None:
+                generated = embed
+            else:
+                if tokens is None:
+                    tokens = torch.tensor(tokenizer.encode(prompt))
+                    tokens = tokens.unsqueeze(0).to(device)
+
+                generated = model.gpt.transformer.wte(tokens)
+
+            for i in range(entry_length):
+
+                outputs = model.gpt(inputs_embeds=generated)
+                logits = outputs.logits
+                logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+                sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+                cumulative_probs = torch.cumsum(nnf.softmax(sorted_logits, dim=-1), dim=-1)
+                sorted_indices_to_remove = cumulative_probs > top_p
+                sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
+                                                    ..., :-1
+                                                    ].clone()
+                sorted_indices_to_remove[..., 0] = 0
+
+                indices_to_remove = sorted_indices[sorted_indices_to_remove]
+                logits[:, indices_to_remove] = filter_value
+                next_token = torch.argmax(logits, -1).unsqueeze(0)
+                next_token_embed = model.gpt.transformer.wte(next_token)
+                # if tokens is None:
+                #     tokens = next_token
+                # else:
+                #     tokens = torch.cat((tokens, next_token), dim=1)
+                generated = torch.cat((generated, next_token_embed), dim=1)
+                if stop_token_index == next_token.item():
+                    break
+
+            output_list = list(tokens.squeeze().cpu().numpy())
+            output_text = tokenizer.decode(output_list)
+            generated_list.append(output_text)
+
+    return generated_list[0]
+
+
+def generate_first_feature_caption(prefix, model: ClipCaptionModel, prefix_length=10):
     gpt_embedding_size = model.gpt.transformer.wte.weight.shape[1]
 
     embed = model.clip_project(prefix).view(-1, prefix_length, gpt_embedding_size)
 
-    return embed.view(embed.shape[0], -1)
+    filter_value = -float("Inf")
+    model.eval()
+    entry_length = 67
+    temperature = 1.0
+    top_p = 0.8
+    stop_token: str = '.'
+    stop_token_index = model.tokenizer.encode(stop_token)[0]
+    losses = []
+    tokens = None
+    generated_list = []
+    break_flag = False
+    out_features = None
+    res = []
+
+    for entry_idx in range(len(embed)):
+        generated = embed[entry_idx].unsqueeze(0)
+        tokens = None
+        break_flag = False
+        out_features = None
+
+
+        for i in range(entry_length):
+
+
+            # print(i)
+            features = model.gpt(inputs_embeds=generated)
+
+            out_features = model.activation['first_layer']
+
+            logits = features.logits
+
+            logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+
+            sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+            cumulative_probs = torch.cumsum(nnf.softmax(sorted_logits, dim=-1), dim=-1)
+            sorted_indices_to_remove = cumulative_probs > top_p
+            sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
+                                                ..., :-1
+                                                ].clone()
+            sorted_indices_to_remove[..., 0] = 0
+
+            indices_to_remove = sorted_indices[sorted_indices_to_remove]
+            logits[:, indices_to_remove] = filter_value
+            next_token = torch.argmax(logits, -1).unsqueeze(0)
+            next_token_embed = model.gpt.transformer.wte(next_token)
+            if tokens is None:
+                tokens = next_token
+            else:
+                tokens = torch.cat((tokens, next_token), dim=1)
+            generated = torch.cat((generated, next_token_embed), dim=1)
+
+            if stop_token_index == next_token.item():
+                res.append(out_features[:, -1, :])
+                out_features = None
+                break
+
+        output_list = list(tokens.squeeze().cpu().numpy())
+        output_text = model.tokenizer.decode(output_list)
+        generated_list.append(output_text)
+        if out_features is not None:
+            res.append(out_features[:, -1, :])
+
+    return res,  generated_list
