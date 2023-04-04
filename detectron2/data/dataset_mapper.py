54eb5abe4c3f52f4d39c75d7448bd8f36236ee31
sina malakouti <siinamalakouti@gmail.com>
2023-04-04 17:39:04 -0400

    Update dataset_mapper.py

--- detectron2/data/dataset_mapper.py
+++ detectron2/data/dataset_mapper.py
@@ -464,7 +464,7 @@ class DatasetMapperTwoCropSeparate(DatasetMapper):
         """
         dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
         image = utils.read_image(dataset_dict["file_name"], format=self.img_format)
-        utils.check_image_size(dataset_dict, image)
+        # utils.check_image_size(dataset_dict, image)
 
         if "clipart_dt_file_name" in dataset_dict:
             image_src2trgt = utils.read_image(dataset_dict["clipart_dt_file_name"], format=self.img_format)
@@ -481,7 +481,7 @@ class DatasetMapperTwoCropSeparate(DatasetMapper):
         else:
             sem_seg_gt = None
 
-        aug_input = T.AugInput(image, sem_seg=sem_seg_gt)
+        aug_input = T.StandardAugInput(image, sem_seg=sem_seg_gt)
         transforms = aug_input.apply_augmentations(self.augmentation)
         image_weak_aug, sem_seg_gt = aug_input.image, aug_input.sem_seg
 
