2ed9b077fa28c5a0441b58cc259844104ef53765
sina malakouti <siinamalakouti@gmail.com>
2023-04-10 14:03:06 -0400

    Update train_loop.py

--- detectron2/engine/train_loop.py
+++ detectron2/engine/train_loop.py
@@ -311,12 +311,12 @@ class SimpleTrainer(TrainerBase):
         loss = {}
         #for l in loss_dict:
             #loss_dict[l] = loss_dict[l] * 0.0
-        if self.iter > 10100 :
+        if self.iter > 1000 :
 
-            caption_consistency_loss = self.model(data, clipcap_model=self.clipcap_model, branch='caption_consistency')
-            loss['cont_loss'], loss['kd_loss'] = caption_consistency_loss
-            # region_consistency_loss = self.model(data, clipcap_model=self.clipcap_model, branch='caption_consistency_regionLevel')
-            # loss['cont_region'] = region_consistency_loss
+            # caption_consistency_loss = self.model(data, clipcap_model=self.clipcap_model, branch='caption_consistency')
+            # loss['cont_loss'], loss['kd_loss'] = caption_consistency_loss
+            region_consistency_loss = self.model(data, clipcap_model=self.clipcap_model, branch='caption_consistency_regionLevel')
+            loss['cont_region'] = region_consistency_loss
         else:
             caption_consistency_loss = self.model(data, clipcap_model=self.clipcap_model, branch='caption_consistency')
             loss['cont_loss'], loss['kd_loss'] = caption_consistency_loss[0] * 0.0, caption_consistency_loss[0] * 0.0
